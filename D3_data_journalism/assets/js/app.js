c2c9ea96277c362f12b79978f628f605fb18b18b
Tri Nguyen <snico90@gmail.com>
2019-11-11 10:57:34 -0500

    update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,174 @@
-// @TODO: YOUR CODE HERE!
+// function that automatically resizes the chart
+function makeResponsive() {
+    // if the svg area is not empty when browser loads, remove it and resize version of chart
+    var svgArea = d3.select("body").select("svg");
+
+    // clear svg if not empty
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+    // svg wrapper dimensions are determined by the current width and height of browser
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+
+    var margin = {
+        top: 50,
+        bottom: 50,
+        right: 50,
+        left: 50
+    };
+
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
+    // append svg element
+
+    var svg = d3
+        .select("#scatter")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+    
+    // append group element
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    // read csv
+    d3.csv("../../data/data.csv").then(function(healthData) {
+        
+        // parse data
+        healthData.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.smokesHigh = +data.smokesHigh;
+        });
+
+
+    // Create scale functions
+    var yLinearScale = d3.scaleLinear().range([height, 0]);
+    var xLinearScale = d3.scaleLinear().range([0, width]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Scale the domain
+    var xMin;
+    var xMax;
+    var yMin;
+    var yMax;
+
+    xMin = d3.min(healthData, function(data) {
+        return +data.poverty * 0.95;
+    });
+
+    xMax = d3.max(healthData, function(data) {
+        return +data.poverty * 1.05;
+    });
+
+    yMin = d3.min(healthData, function(data) {
+        return +data.smokesHigh * 0.98;
+    });
+    
+    yMax = d3.max(healthData, function(data) {
+        return +data.smokesHigh *1.02;
+    });
+    
+    xLinearScale.domain([xMin, xMax]);
+    yLinearScale.domain([yMin, yMax]);
+
+    
+    // Step 1: Initialize tooltip 
+        var toolTip = d3
+        .tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(data) {
+            var stateName = data.state;
+            var pov = +data.poverty;
+            var smokesHigh = +data.smokesHigh;
+            return (
+                stateName + '<br> Poverty: ' + pov + '% <br> Smokes High: ' + smokesHigh +'%'
+            );
+        });
+
+    // Step 2: Create tooltip
+    chartGroup.call(toolTip);
+
+    chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(data, index) {
+            return xLinearScale(data.poverty)
+        })
+        .attr("cy", function(data, index) {
+            return yLinearScale(data.smokesHigh)
+        })
+        .attr("r", "15")
+        .attr("fill", "lightblue")
+        // display tooltip on click
+        .on("mouseenter", function(data) {
+            toolTip.show(data);
+        })
+        // hide tooltip on mouseout
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+    
+    // Appending a label to each data point
+    chartGroup.append("text")
+        .style("text-anchor", "middle")
+        .style("font-size", "12px")
+        .selectAll("tspan")
+        .data(healthData)
+        .enter()
+        .append("tspan")
+            .attr("x", function(data) {
+                return xLinearScale(data.poverty - 0);
+            })
+            .attr("y", function(data) {
+                return yLinearScale(data.smokesHigh - 0.2);
+            })
+            .text(function(data) {
+                return data.abbr
+            });
+    
+    // Append an SVG group for the xaxis, then display x-axis 
+    chartGroup
+        .append("g")
+        .attr('transform', `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // Append a group for y-axis, then display it
+    chartGroup.append("g").call(leftAxis);
+
+    // Append y-axis label
+    chartGroup
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0-margin.left + 40)
+        .attr("x", 0 - height/2)
+        .attr("dy","1em")
+        .attr("class", "axis-text")
+        .text("High Smokes (%)")
+
+    // Append x-axis labels
+    chartGroup
+        .append("text")
+        .attr(
+            "transform",
+            "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
+        )
+        .attr("class", "axis-text")
+        .text("In Poverty (%)");
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+// when browser loads, makeResponsive() is called
+makeResponsive();
+
+// when the browser window is resized, makeResponsive() is called. 
+d3.select(window).on("resize", makeResponsive);
